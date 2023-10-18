709aa4d5ac5fb2f3b0f474f775d3288dc44de4f0
Thomas Kimble <100125668+ThomasOliverKimble@users.noreply.github.com>
2023-10-18 20:58:17 +0200

    add coming soon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fixed top-0 left-0 w-full h-full bg-[#0000FF] flex justify-center items-center flex-col">
+      <img src="/logo.svg" alt="Logo" className="max-w-full h-auto" />
+      <p className="text-[#FFFDD0] text-7xl font-open-sans font-extrabold ml-10 -mt-12">coming soon</p>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}", "./public/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
