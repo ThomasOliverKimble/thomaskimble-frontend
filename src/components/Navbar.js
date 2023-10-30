4d0caf132f6c0efa091908acfd719d59064392ac
ThomasOliverKimble <tokimble@gmail.com>
2023-10-30 22:14:03 +0100

    update navbar for safari

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,41 +5,29 @@ import './Navbar.css';
 function Navbar() {
   const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const navbarHandler = () => setClick(!click);
 
   return (
     <>
       <nav className='navbar'>
-        <div className={click ? 'navbar-background active' : 'navbar-background'}></div>
-        <div className={click ? 'navbar-container active' : 'navbar-container'}>
-          <div className='navbar-top'>
-            <div className={click ? 'navbar-top-background active' : 'navbar-top-background'}></div>
-            <div className={click ? 'menu-icon active' : 'menu-icon'} onClick={handleClick}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <Link to='/' className={click ? 'navbar-logo active' : 'navbar-logo'} onClick={closeMobileMenu}></Link>
-          </div>
-          <ul className={click ? 'navbar-menu active' : 'navbar-menu'}>
-            <li className='navbar-item'>
-              <Link to='/' className='navbar-links' onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className='navbar-item'>
-              <Link to='/projects' className='navbar-links' onClick={closeMobileMenu}>
-                Projects
-              </Link>
-            </li>
-            <li className='navbar-item'>
-              <Link to='/about' className='navbar-links' onClick={closeMobileMenu}>
-                About
-              </Link>
-            </li>
+        <div className={'navbar-hamburger ' + (click ? 'active' : '')} onClick={navbarHandler}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={'navbar-logo ' + (click ? 'active' : '')}>
+          <div className={'navbar-logo-svg navbar-logo-cream ' + (click ? 'active' : '')}/>
+          <div className={'navbar-logo-svg navbar-logo-blue ' + (click ? 'active' : '')}/>
+        </div>
+        <div className={'navbar-background ' + (click ? 'active' : '')} />
+        <div className={'navbar-menu ' + (click ? 'active' : '')} >
+          <ul className='navbar-menu-items'>
+            <li><Link onClick={navbarHandler} to='/'>Home</Link></li>
+            <li><Link onClick={navbarHandler} to='/projects'>Projects</Link></li>
+            <li><Link onClick={navbarHandler} to='/about'>About</Link></li>
           </ul>
         </div>
+        <div className={'navbar-blur ' + (click ? 'active' : '')}></div>
       </nav>
     </>
   );
