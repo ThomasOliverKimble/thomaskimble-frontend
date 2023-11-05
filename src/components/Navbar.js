30d09d43cb9bfacb6e98fb5b12ef341c5753ab81
ThomasOliverKimble <tokimble@gmail.com>
2023-11-05 16:07:32 +0100

    fix navbar and make mobile responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,36 +6,61 @@ function Navbar() {
   const [navbarClick, setNavbarClick] = useState(false);
   const [scrollPosition, setScrollPosition] = useState(false);
 
-  const handleNavbarClick = () => setNavbarClick(!navbarClick);
+  const handleNavbarClick = () => {
+    setNavbarClick(!navbarClick);
+    if (navbarClick) {
+      document.body.style.overflow = "auto"
+    } else {
+      document.body.style.overflow = "hidden"
+    }
+  }
   const handleScroll = () => setScrollPosition(window.scrollY);
+
+  const checkWidth = () => {
+    document.body.style.overflow = "auto"
+    if (!navbarClick) {
+      setNavbarClick(navbarClick);
+    }
+  }
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  useEffect(() => {
+    window.addEventListener('resize', checkWidth);
+    return () => window.removeEventListener('resize', checkWidth);
+  }, []);
+
   return (
     <>
-      <nav className='navbar'>
-        <div className={'navbar-top ' + (scrollPosition != 0 ? 'scroll' : '')}/>
-        <div className={'navbar-hamburger ' + (navbarClick ? 'active' : '')} onClick={handleNavbarClick}>
-          <span></span>
-          <span></span>
-          <span></span>
+      <nav className={'navbar ' + (navbarClick ? 'active ' : '') + (scrollPosition != 0 ? 'scroll' : '')}>
+        <div className='navbar-top'>
+          <div className={'navbar-top-background '}/>
+          <div className={'navbar-top-border '}/>
+          <div className='navbar-top-container'>
+            <div className={'navbar-top-container-hamburger '} onClick={handleNavbarClick}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className='navbar-top-container-logo'>
+              <div className='navbar-top-container-logo-svg navbar-top-container-logo-blue-no-text'></div>
+              <div className='navbar-top-container-logo-svg navbar-top-container-logo-cream'/>
+              <div className='navbar-top-container-logo-svg navbar-top-container-logo-blue'/>
+            </div>
+          </div>
         </div>
-        <div className={'navbar-logo ' + (navbarClick ? 'active' : '')}>
-          <div className={'navbar-logo-svg navbar-logo-cream ' + (navbarClick ? 'active' : '')}/>
-          <div className={'navbar-logo-svg navbar-logo-blue ' + (navbarClick ? 'active' : '')}/>
-        </div>
-        <div className={'navbar-background ' + (navbarClick ? 'active' : '')} />
-        <div className={'navbar-menu ' + (navbarClick ? 'active' : '')} >
+        <div className='navbar-background'/>
+        <div className='navbar-menu'>
           <ul className='navbar-menu-items'>
             <li><Link onClick={handleNavbarClick} to='/'>Home</Link></li>
             <li><Link onClick={handleNavbarClick} to='/projects'>Projects</Link></li>
             <li><Link onClick={handleNavbarClick} to='/about'>About</Link></li>
           </ul>
         </div>
-        <div className={'navbar-blur ' + (navbarClick ? 'active' : '')}></div>
+        <div className='navbar-blur'></div>
       </nav>
     </>
   );
