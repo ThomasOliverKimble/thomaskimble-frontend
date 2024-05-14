e7553d10a2f467e4e9d00b83a566b496acbc54d0
ThomasOliverKimble <tokimble@gmail.com>
2024-05-14 17:26:20 +0100

    add md to public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}", "./public/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}", "./public/**/*.{html,js,jsx,md}"],
   theme: {
     extend: {
       fontFamily: {
