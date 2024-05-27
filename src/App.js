2754ac07a20d8c463cde5aec5ea8f204fb44ae3e
ThomasOliverKimble <tokimble@gmail.com>
2024-05-27 15:40:26 +0200

    update coming soon page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import React from "react";
 
 function App() {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-[#0000FF] flex justify-center items-center flex-col">
-      <img src="/logo.svg" alt="Logo" className="max-w-full h-auto" />
-      <p className="text-[#FFFDD0] text-7xl font-open-sans font-extrabold ml-10 -mt-12">coming soon</p>
+    <div className="fixed top-0 left-0 w-full h-full bg-[#0000FF] flex justify-center items-center">
+      <div
+        id="container"
+        className="w-fit h-auto p-10 flex flex-col items-center"
+      >
+        <img src="/logo.svg" alt="Logo" className="max-w-full h-auto" />
+        <p className="relative text-[#FFFDD0] m-auto text-4xl -mt-6 sm:text-7xl font-open-sans font-extrabold sm:-mt-12 text-center">
+          coming soon
+        </p>
+        <p className="text-[#FFFDD0] mt-10 text-center">
+          in the meantime, check out the
+        </p>
+        <a
+          href="https://legacy.thomaskimble.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="text-[#0000FF] bg-[#FFFDD0] font-bold py-2 px-4 rounded mt-2">
+            legacy website
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
